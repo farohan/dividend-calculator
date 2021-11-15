92e9970c989b41bc071d14b184b565c8f02cba04
farohan <68710283+farohan@users.noreply.github.com>
2021-11-14 19:02:46 -0800

    Update play.js

--- play.js
+++ play.js
@@ -4,17 +4,12 @@ const entireForm = document.getElementById('div-yield-form');
 //Selecting submit button
 const submit = document.getElementById('submit-btn');
 
-//Selecting the inputs
-const initField = document.getElementById('initial-inv');
-const yieldInput = document.getElementById('yield');
-const durationInput = document.getElementById('inv-duration');
-
 //Selecting the results box and inside paragraph
 const resultBox = document.getElementById('result-box');
 const results = document.getElementById('results');
 
 //Number of times the dividends are compounded
-const timesCompounded = 4;
+const n = 4;
 
 //To stop form from refreshing page
 function handleForm(event) {
@@ -28,9 +23,13 @@ entireForm.addEventListener('submit', handleForm);
 let finalAmount; 
 
 function dividendCompound() {
-    finalAmount = initField.value * Math.pow((1 + yieldInput.value) / timesCompounded, durationInput.value * timesCompounded);
+    const initial = document.getElementById('initial-inv').value;
+    const divYield = document.getElementById('yield').value;
+    const years = document.getElementById('inv-duration').value;
+    finalAmount = initial * (Math.pow((1 + (divYield / n)), (n * years)));
+    console.log(finalAmount);
     resultBox.style.display = 'block';
-    results.innerHTML = `By the end of ${durationInput.value} quarters, you will have about $${Math.floor(finalAmount)}`;
+    results.innerHTML = `By the end of ${years} year(s), you will have about $${Math.floor(finalAmount)}`;
 }
 
 submit.addEventListener('click', dividendCompound);
